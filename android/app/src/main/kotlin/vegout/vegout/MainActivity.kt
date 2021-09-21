c14a00ca1dbc2b1849487137ab99452333a08136
kayereeves <kiannarachelmarie@gmail.com>
2021-09-20 22:48:49 -0700

    9/20 skeleton UI

--- android/app/src/main/kotlin/vegout/vegout/MainActivity.kt
+++ android/app/src/main/kotlin/vegout/vegout/MainActivity.kt
@@ -3,4 +3,6 @@ package vegout.vegout
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    //final Button navButton = findViewById(R.id.navButton_id);
+    //final Button aboutButton = findViewById(R.id.aboutButton_id);
 }
